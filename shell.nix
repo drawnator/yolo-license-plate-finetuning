bdf9b177e3f78b407797a72cf2117da3ee179344
guilherme <thedrawnator@gmail.com>
2026-05-24 20:38:45 -0300

    ALPR dataset

---
+++ shell.nix
@@ -0,0 +1,15 @@
+# shell.nix
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.mkShell {
+  packages = [
+    (pkgs.python3.withPackages (python-pkgs: with python-pkgs; [
+      # select Python packages here
+      numpy
+      ipykernel
+      matplotlib
+      pip
+      jupyter
+      notebook
+    ]))
+  ];
+}
