9b2b21f1bcf6c9a86e0c0955315e653b5418383f
guilherme <thedrawnator@gmail.com>
2026-05-24 22:15:16 -0300

    yolo26s 100 epochs

--- training/train_yolov26.py
+++ training/train_yolov26.py
@@ -18,10 +18,10 @@ from ultralytics import YOLO
 def train(
     data: str = r"./data.yaml",
     # data: str = r"datasets\UFPR-ALPR dataset\data.yaml",
-    model: str = "	yolo26x.pt",
+    model: str = "	yolo26s.pt",
     # img_size: int = 1920,
     batch_size: int = 16,
-    epochs: int = 200,
+    epochs: int = 100,
     device: str = "0",
     project: str = "runs/train",
     name: str = "yolov26_license_plate",
